82dce4bde3bf199ab3b683fdd144c89c872ac1d9
kunal <kunalrisaanva12@gmail.com>
2024-03-06 14:52:05 +0530

    configured cluster

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import dotenv from "dotenv";
 import connectDB from "./db/db.js";
 import { app } from "./app.js";
+import http from "http";
+import os from "os"
+
+import cluster from "cluster"
+
+
+const lengthOfCPUos = os.cpus().length
+
 
 dotenv.config({
   path: "./.env",
@@ -8,20 +16,39 @@ dotenv.config({
 
 // Database connection
 
-connectDB()
-  .then(() => {
-    app.listen(process.env.PORT || 6000, () => {
-      console.log(`🗿 ⚙️ Server is running on PORT ${process.env.PORT}`);
-    });
-   
-    app.on("error", (err) => {
-      console.log("error", err);
-    });
-    
-  })
-  .catch((err) => {
-    console.log(`MONGO DB connection failed !!!`, err);
+if (cluster.isPrimary) {
+  console.log(`Primary ${process.pid} is running`);
+
+  // Fork workers.
+  for (let i = 0; i < lengthOfCPUos; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
   });
+} else {  
+
+connectDB()
+.then(() => {
+  // Start the Express server
+  const server = http.createServer(app);
+
+  server.listen(process.env.PORT || 4000, () => {
+    console.log(`\n ⚙️ Server is running on ${process.env.PORT || 4000} And PID ${process.pid} ⚙️`);
+  });
+
+  app.on('error', (err) => {
+    // Handle uncaught exceptions or errors here
+    console.error('An error occurred:', err);
+  });
+})
+.catch((error) => {
+  console.log("MONGODB CONNECTION FAILED !! :", error);
+});
+
+}
+
 
 
 
